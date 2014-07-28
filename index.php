59ac61d92b536cb9aaf996d14b440e6f94a4ef9a
Robertyi <robertyi@126.com>
2014-07-28 20:29:12 +0800

    first commit
    Signed-off-by: Robertyi <robertyi@126.com>

---
+++ index.php
@@ -0,0 +1,62 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="utf-8">
+<link rel="shortcut icon" type="image/png" href="http://www.dianjoy.com/wp-content/themes/dian2013/img/logo.png">
+
+    <title>竞猜-管理员服务</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom styles for this template -->
+    <link href="css/dashboard.css" rel="stylesheet">
+
+  </head>
+
+  <body>
+
+    <div class="navbar navbar-inverse navbar-fixed-top" role="navigation">
+      <div class="container-fluid">
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target=".navbar-collapse">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+          <a class="navbar-brand" href="">竞猜</a>
+        </div>
+        <div class="navbar-collapse collapse">
+          <ul class="nav navbar-nav navbar-right">
+            <li><a href="">Dashboard</a></li>
+            <li><a href="">Settings</a></li>
+            <li><a href="">Profile</a></li>
+            <li><a href="">Help</a></li>
+          </ul>
+          <form class="navbar-form navbar-right">
+            <input type="text" class="form-control" placeholder="Search...">
+          </form>
+        </div>
+      </div>
+    </div>
+
+    <div class="container-fluid">
+      <div class="row">
+        <div class="col-sm-3 col-md-2 sidebar">
+          <ul class="nav nav-sidebar">
+            <li class="active"><a href="index.php">ALL</a></li>
+            <li><a href="lol.php">英雄联盟</a></li>
+            <li><a href="spread3.html">DOTA2</a></li>
+            <li><a href="spread4.html">炉石传说</a></li>
+            <li><a href="spread4.html">星际争霸2</a></li>
+          </ul>
+        </div>
+        <div class="col-sm-9 col-sm-offset-3 col-md-10 col-md-offset-2 main">
+
+
+        </div>
+        </div>
+      </div>
+</body>
+</html>
---
+++ lol.php
@@ -0,0 +1,132 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="utf-8">
+<link rel="shortcut icon" type="image/png" href="http://www.dianjoy.com/wp-content/themes/dian2013/img/logo.png">
+
+    <title>竞猜-管理员服务</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom styles for this template -->
+    <link href="css/dashboard.css" rel="stylesheet">
+
+  </head>
+
+  <body>
+
+    <div class="navbar navbar-inverse navbar-fixed-top" role="navigation">
+      <div class="container-fluid">
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target=".navbar-collapse">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+          <a class="navbar-brand" href="">竞猜</a>
+        </div>
+        <div class="navbar-collapse collapse">
+          <ul class="nav navbar-nav navbar-right">
+            <li><a href="">Dashboard</a></li>
+            <li><a href="">Settings</a></li>
+            <li><a href="">Profile</a></li>
+            <li><a href="">Help</a></li>
+          </ul>
+          <form class="navbar-form navbar-right">
+            <input type="text" class="form-control" placeholder="Search...">
+          </form>
+        </div>
+      </div>
+    </div>
+
+    <div class="container-fluid">
+      <div class="row">
+        <div class="col-sm-3 col-md-2 sidebar">
+          <ul class="nav nav-sidebar">
+            <li><a href="index.php">ALL</a></li>
+            <li class="active"><a href="lol.php">英雄联盟</a></li>
+            <li><a href="spread3.html">DOTA2</a></li>
+            <li><a href="spread4.html">炉石传说</a></li>
+            <li><a href="spread4.html">星际争霸2</a></li>
+          </ul>
+        </div>
+        <div class="col-sm-9 col-sm-offset-3 col-md-10 col-md-offset-2 main">
+
+          <div class="row placeholders">
+            <div class="col-xs-6 col-sm-3 placeholder">
+              <img src="image/.jpg">
+              <h4>英雄联盟</h4>
+              <span class="text-muted">Copyright ©龙图游戏</span>
+            </div>
+            <div class="col-xs-18 col-sm-9 placeholder">
+            <br /><br />
+              <textarea id = "null" class="form-control" rows="4" >推广内容</textarea>
+              <br />
+              <button type="button" class="btn btn-primary btn-lg btn-block" onclick="CopyValue()">复制内容</button>
+            </div>
+          </div>
+            <div class="col-xs-6 col-sm-3 " style="text-align:center">
+
+              <div class="btn-group-vertical">
+              <button type="button" class="btn btn-default" onclick="window.open('http://tieba.baidu.com/f?kw=%B5%B6%CB%FE%B4%AB%C6%E6')">百度贴吧</button>
+              <button type="button" class="btn btn-primary" onclick="window.open('http://bbs.mgamer.cn/forum-2132-1.html')">360</button>
+              <button type="button" class="btn btn-success" onclick="window.open('http://bbs.ptbus.com/forum-1316-1.html')">口袋巴士</button>
+              <button type="button" class="btn btn-info" onclick="window.open('http://bbs.4399.cn/forums-mtag-81984')">4399</button>
+              <button type="button" class="btn btn-warning" onclick="window.open('http://bbs.18183.com/forum-daotachuanqi-1.html')">18183</button>
+              <button type="button" class="btn btn-danger" onclick="window.open('#">爱游戏</button>
+              </div>
+
+            </div>
+            <div class="col-xs-18 col-sm-9 ">
+
+          <div class="table-responsive">
+            <table class="table table-striped">
+              <thead>
+                <tr>
+                  <th>QQ群号</th>
+                  <th>QQ群名</th>
+                  <th>QQ群人数</th>
+                  <th></th>
+                  <th></th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <td>326107072</td>
+                  <td></td>
+                  <td></td>
+                  <td></td>
+                  <td></td>
+                </tr>
+                <tr>
+                  <td>138068534</td>
+                  <td></td>
+                  <td></td>
+                  <td></td>
+                  <td></td>
+                </tr>
+                <tr>
+                  <td>164183521</td>
+                  <td></td>
+                  <td></td>
+                  <td></td>
+                  <td></td>
+                </tr>
+                <tr>
+                  <td>264520084</td>
+                  <td></td>
+                  <td></td>
+                  <td></td>
+                  <td></td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
+        </div>
+        </div>
+      </div>
+    </div> 
+</body>
+</html>
